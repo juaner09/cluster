8d85c0c9eb3fe25daef483e7ec33f9fc20f8b5f4
bettyli <bettyli@timerchina.com>
2017-05-21 18:33:26 +0800

    init

---
+++ src/main/java/com/timerchina/recommender/SimilarityMatrixCache.java
@@ -0,0 +1,39 @@
+package com.timerchina.recommender;
+
+import java.io.File;
+
+import com.timerchina.similarity.naive.SimilarityMatrix;
+
+public class SimilarityMatrixCache {
+
+    private Store store;
+    private String location;
+    
+    public SimilarityMatrixCache(File location) {
+        store = new FileStore(location);
+        this.location = location.getAbsolutePath();
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
+    public void put(String id, SimilarityMatrix similarityMatrix) {
+        if( store.exists(id) ) {
+            store.remove(id);
+        }
+        store.put(id, similarityMatrix);
+    }
+    
+    public SimilarityMatrix get(String id) {
+        SimilarityMatrix s = null;
+        if( store.exists(id) ) {
+            s = (SimilarityMatrix)store.get(id);
+        }
+        return s;
+    }
+    
+    public void remove(String id) {
+        store.remove(id);
+    }
+}
---
+++ src/main/java/com/timerchina/similarity/ch4/Distance.java
@@ -0,0 +1,6 @@
+package com.timerchina.similarity.ch4;
+
+
+public interface Distance {
+    double getDistance(double[] x, double[] y);
+}
